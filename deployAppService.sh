e8bc8e5fb15d99b1fa653b7cf6be682ca52254bf
Julio Solarte <solarte.julio@gmail.com>
2021-04-29 20:48:12 +0200

    Random fixes

--- deployAppService.sh
+++ deployAppService.sh
@@ -4,6 +4,6 @@ echo on
 az --version
 
 #Create webapp based on Docker Compose
-if [ ! az webapp show --name "$(WEBAPPNAME)" --resource-group "$(RESOURCEGROUP)" ]; then
-    az webapp create --resource-group "$(RESOURCEGROUP)" --plan "$(APPSERVICEPLAN)" --name "$(WEBAPPNAME)" --multicontainer-config-type compose --multicontainer-config-file docker-compose.yml
+if [ ! az webapp show --name "$WEBAPPNAME" --resource-group "$RESOURCEGROUP" ]; then
+    az webapp create --resource-group "$RESOURCEGROUP" --plan "$APPSERVICEPLAN" --name "$WEBAPPNAME" --multicontainer-config-type compose --multicontainer-config-file docker-compose.yml
 fi
