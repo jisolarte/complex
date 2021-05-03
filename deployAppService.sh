374666cdea28e8560cef4cd7f3ff3175854b3d91
Julio Solarte <solarte.julio@gmail.com>
2021-05-03 10:04:17 +0200

    Random fixes

--- deployAppService.sh
+++ deployAppService.sh
@@ -4,6 +4,6 @@ echo on
 az --version
 
 #Create webapp based on Docker Compose
-if [ ! az webapp show --name "react-webapp" --resource-group "react-app-rg" ]; then
+if ! [ az webapp show --name "react-webapp" --resource-group "react-app-rg" ]; then
     az webapp create --resource-group "react-app-rg" --plan "react-app-service-plan" --name "react-webapp" --multicontainer-config-type 'compose' --multicontainer-config-file 'docker-compose.yml'
 fi
